183b6b0070c458bc44da68374ce8c6e3b7c48e3a
atreutel <austin.treutel@gmail.com>
2018-11-15 17:57:13 -0500

    using bs4

--- investing/stock/mitools.py
+++ investing/stock/mitools.py
@@ -1,27 +1,35 @@
 from lxml import html
 import requests
 from urllib.request import urlopen
-import xmltodict
 from xml.dom import minidom
-import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup as soup
+
+
+def getPage(url):
+	client = urlopen(url)
+	page = client.read()
+	client.close()
+
+	return soup(page, "html.parser")
 
 def getRequest(href):
-    page = requests.get(href)
-    return html.fromstring(page.content)
+	page = requests.get(href)
+	return html.fromstring(page.content)
 
 def getCompanyFromSymbol(symbol):
-	tree = getRequest("https://www.sec.gov/cgi-bin/browse-edgar?CIK=" + symbol)
-	name = tree.xpath('//*[@class="companyName"]/text()')[0]
-	cik = tree.xpath('//*[@class="companyName"]/a/text()')[0].split(' ')[0]
-	result = []
-	if name and cik:
-		result = [name,cik]
-	return result
+	page_soup = getPage("https://www.sec.gov/cgi-bin/browse-edgar?CIK=" + symbol)
+	try:
+		comp_name = page_soup.find(class_="companyName")
+		name = [text for text in comp_name.stripped_strings][0]
+		cik = comp_name.a.get_text().split(" ")[0]
+		return [name,cik]
+	except:
+		return
 
 def getFilings(name,cik,count):
 	url = getFilingsUrl(cik, '10-K',count)
-	tree = getRequest(url)
-	filings = getReports(tree,count)
+	page_soup = getPage(url)
+	filings = getReports(page_soup,count)
 	return filings
 
 def getFilingsUrl(cik, filetype, count):
@@ -31,42 +39,38 @@ def getFilingsUrl(cik, filetype, count):
 def buildFilings(tree, count=1):
 	reports = getReports(tree, count)
 	return reports
-	#result = "<table><tr><td>Quarter</td><td>URL</td><td>Liabilities</td></tr>"
-	#for report in reports:
-	#	result += '<tr><td>' + str(report.quarter) + '</td><td>' + report.url + '</td><td>' + str(report.liabilities) + '</td></tr>'
-	#result += '</table>'
-	#return result
 
 class filingsReport(object):
 	url = ''
 
-def getReports(tree, count=1):
+def getReports(page_soup, count=1):
 	baseurl = "https://www.sec.gov"
-	elems = tree.xpath('//*[@id="documentsbutton"]')[:count]
+	elems = page_soup.find_all(id="documentsbutton", limit=count)
 	result = []
 	for elem in elems:
-		url = baseurl + elem.attrib["href"]
-		contentPage = getRequest(url)
-		url = baseurl + contentPage.xpath('//*[@id="formDiv"]/div/table[@summary="Data Files"]/tr[2]/td[3]/a')[0].attrib["href"]
-		dom = minidom.parse(urlopen(url))
+		url = baseurl + elem['href']
+		index_page = getPage(url)
+		url = baseurl + index_page.find(summary="Data Files").find_all('tr')[1].a['href']
+		doc_soup = getDoc(url)
+		#dom = minidom.parse(urlopen(url))
 		report = filingsReport()
 		report.url = url
-		report.fiscalYear = str(getReportedValue(dom,'dei:DocumentFiscalYearFocus'))
-		report.fiscalPeriod = str(getReportedValue(dom,'dei:DocumentFiscalPeriodFocus'))
+		report.fiscalYear = str(getReportedValue(doc_soup,'dei:DocumentFiscalYearFocus'))
+		report.fiscalPeriod = str(getReportedValue(doc_soup,'dei:DocumentFiscalPeriodFocus'))
 		report.period = report.fiscalPeriod + report.fiscalYear
-		report.liabilities = getReportedValue(dom,'us-gaap:Liabilities')
-		report.eps = getReportedValue(dom,'us-gaap:EarningsPerShareDiluted')
-		#report.cash = getReportedValue(dom,'us-gaap:CashAndCashEquivalentsAtCarryingValue')
-		report.cash = getReportedValue(dom,'us-gaap:CashCashEquivalentsAndShortTermInvestments')
-		report.sales = getReportedValue(dom,'us-gaap:RevenueFromContractWithCustomerExcludingAssessedTax')
-		report.shequity = getReportedValue(dom,'us-gaap:StockholdersEquity')
-		report.shares = getReportedValue(dom,'us-gaap:CommonStockSharesOutstanding')
+		report.liabilities = getReportedValue(doc_soup,'us-gaap:Liabilities')
+		report.eps = getReportedValue(doc_soup,'us-gaap:EarningsPerShareDiluted')
+		#report.cash = getReportedValue(doc_soup,'us-gaap:CashAndCashEquivalentsAtCarryingValue')
+		report.cash = getReportedValue(doc_soup,'us-gaap:CashCashEquivalentsAndShortTermInvestments')
+		report.sales = getReportedValue(doc_soup,'us-gaap:RevenueFromContractWithCustomerExcludingAssessedTax')
+		report.shequity = getReportedValue(doc_soup,'us-gaap:StockholdersEquity')
+		report.shares = getReportedValue(doc_soup,'us-gaap:CommonStockSharesOutstanding')
 		if report.shequity and report.shares:
 			report.bookvalue = int(report.shequity) / int(report.shares)
 		else:
 			report.bookvalue = 'Invalid'
-		report.netincomeloss = getReportedValue(dom,'us-gaap:NetIncomeLoss')
-		report.grossprofit = getReportedValue(dom,'us-gaap:GrossProfit')
+		report.netincomeloss = getReportedValue(doc_soup,'us-gaap:NetIncomeLoss')
+		report.grossprofit = getReportedValue(doc_soup,'us-gaap:GrossProfit')
 
 		result.append(report)
 
@@ -74,7 +78,15 @@ def getReports(tree, count=1):
 		return result[0]
 	return result
 
+def getDoc(url):
+	client = urlopen(url)
+	doc = client.read()
+	client.close()
+
+	return soup(doc, "xml")
+
 def getReportedValue(rpt, elem):
-	if not rpt.getElementsByTagName(elem):
+	try:
+		return rpt.find(elem).get_text()
+	except:
 		return
-	return rpt.getElementsByTagName(elem)[0].firstChild.data
